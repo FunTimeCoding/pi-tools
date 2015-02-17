478f0e06039be68c7717503d17060056d39fe8b3
Alexander Reitzel <funtimecoding@gmail.com>
2015-02-17 20:04:04 +0100

    make create scripts more efficient by not downloading and unpacking every time

--- create-noobs-sd-card.sh
+++ create-noobs-sd-card.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
+
+ZIP="noobs.zip"
+if [ ! -f "${ZIP}" ]; then
+    #wget http://downloads.raspberrypi.org/NOOBS_latest -O ${ZIP}
+    wget http://downloads.raspberrypi.org/NOOBS_lite_latest -O ${ZIP}
+fi
+
+DIR="noobs"
+if [ ! -d "${DIR}" ]; then
+    unzip ${ZIP} -d ${DIR}
+fi
+
 diskutil unmountDisk /dev/disk2
 sudo diskutil eraseDisk FAT32 NOOBS MBRFormat /dev/disk2
-#wget http://downloads.raspberrypi.org/NOOBS_latest -O noobs.zip
-#unzip noobs.zip -d noobs
-wget http://downloads.raspberrypi.org/NOOBS_lite_latest -O noobs-lite.zip
-unzip noobs-lite.zip -d noobs
-cp -R noobs/* /Volumes/NOOBS/
+cp -R ${DIR}/* /Volumes/NOOBS/
 diskutil unmountDisk /dev/disk2
--- create-raspbian-sd-card.sh
+++ create-raspbian-sd-card.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
+
+ZIP="raspbian.zip"
+if [ ! -f "${ZIP}" ]; then
+    wget http://downloads.raspberrypi.org/raspbian_latest -O ${ZIP}
+fi
+
+IMG="raspbian.img"
+if [ ! -f "${IMG}" ]; then
+    unzip ${ZIP} -d ${IMG}
+fi
+
 diskutil unmountDisk /dev/disk2
-wget http://downloads.raspberrypi.org/raspbian_latest -O raspbian.img.zip
-unzip raspbian.img.zip -d raspbian.img
 sudo diskutil partitionDisk /dev/disk2 1 MBR "Free Space" "%noformat%" 100%
-sudo dd bs=1m if=raspbian.img of=/dev/rdisk2
+sudo dd bs=1m if=${IMG} of=/dev/rdisk2
 sudo diskutil eject /dev/rdisk2
