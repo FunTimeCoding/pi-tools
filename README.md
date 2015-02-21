e2addbc35c6e06947a81f1cd02c42d0dd30ed4c2
Alexander Reitzel <funtimecoding@gmail.com>
2015-02-21 16:17:26 +0100

    improve fabric code by using more available features, update readme accordingly

--- README.md
+++ README.md
@@ -3,20 +3,39 @@
 
 ## Setup
 
-Run initial setup on a new pi.
+Most example commands contain the variable `$HOST`, thus making it easy to define the target IP or hostname in front.
 
 ```sh
-fab -u pi -k -H 192.168.2.125 setup
+HOST=192.168.1.100 fab -u pi -k -H $HOST basic_setup
 ```
+
 
 Copy your SSH key to the new pi.
 
 ```sh
-ssh-copy-id -i pi@192.168.2.125
+ssh-copy-id -i pi@$HOST
+```
+
+List available commands.
+
+```sh
+fab -l
+```
+
+Run initial setup on a new pi.
+
+```sh
+fab -u pi -k -H $HOST basic_setup
+```
+
+Run a command which takes an argument.
+
+```sh
+fab -u pi -k -H $HOST change_hostname:hydrogen
 ```
 
 Run an arbitrary command.
 
 ```sy
-fab -u pi -k -H 192.168.2.125 -- uname -a
+fab -u pi -k -H $HOST -- hostname
 ```
--- fabfile.py
+++ fabfile.py
@@ -1,26 +1,39 @@
-from fabric.api import run
+from fabric.api import run, execute, task
+from fabric.utils import error
+import re
 
 
-def host_type():
-    run('uname -s')
-
-
-def path():
-    run('echo $PATH')
-
-
-def setup():
+@task
+def basic_setup():
+    """Upgrade system and install basic tools."""
     run('sudo apt-get update')
     run('sudo apt-get upgrade')
+    run('sudo apt-get install -y htop vim-nox avahi-daemon tmux bc')
 
 
-def basics():
-    run('sudo apt-get install -y htop vim-nox avahi-daemon')
+@task
+def change_hostname(new_hostname=''):
+    """Change the hostname of the target host."""
+    if new_hostname is '':
+        print('Usage: change_hostname:hydrogen')
+    elif is_valid_hostname(new_hostname):
+        print('New hostname: %s' % new_hostname)
+    else:
+        error('Invalid hostname: %s' % new_hostname)
 
 
-def audio():
+@task
+def audio_setup():
+    """Executes basic_setup first, then installs audio tools."""
+    execute(basic_setup)
     run('sudo apt-get install -y sox')
 
 
-def tools():
-    run('sudo apt-get install -y bc tmux')
+def is_valid_hostname(hostname):
+    if len(hostname) > 255:
+        return False
+    if hostname[-1] == ".":
+        # strip exactly one dot from the right, if present
+        hostname = hostname[:-1]
+    allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
+    return all(allowed.match(x) for x in hostname.split("."))
