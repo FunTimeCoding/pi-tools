942c8b04b5d11023efddfefca4eab35f3b5cc696
Alexander Reitzel <funtimecoding@gmail.com>
2015-07-29 09:26:23 +0200

    fix unzip, parametrize disk2 argument, add run style check script, add sha1 check

--- bin/create-noobs-sd-card.sh
+++ bin/create-noobs-sd-card.sh
@@ -1,17 +1,17 @@
 #!/bin/sh -e
 
-SCRIPT_DIR="$(cd $(dirname ${0}); pwd)"
+SCRIPT_DIR=$(cd "$(dirname "${0}")"; pwd)
 WORK_DIR="${SCRIPT_DIR}/../local"
-
 ZIP="${WORK_DIR}/noobs.zip"
+DIR="${WORK_DIR}/noobs"
+
 if [ ! -f "${ZIP}" ]; then
     #wget http://downloads.raspberrypi.org/NOOBS_latest -O ${ZIP}
     wget http://downloads.raspberrypi.org/NOOBS_lite_latest -O ${ZIP}
 fi
 
-DIR="${WORK_DIR}/noobs"
 if [ ! -d "${DIR}" ]; then
-    unzip ${ZIP} -d ${DIR}
+    unzip -p "${ZIP}" "*.img" | cat > "${IMG}"
 fi
 
 diskutil unmountDisk /dev/disk2
--- bin/create-raspbian-sd-card.sh
+++ bin/create-raspbian-sd-card.sh
@@ -1,19 +1,40 @@
 #!/bin/sh -e 
 
-SCRIPT_DIR="$(cd $(dirname ${0}); pwd)"
+SCRIPT_DIR=$(cd "$(dirname "${0}")"; pwd)
 WORK_DIR="${SCRIPT_DIR}/../local"
+ARCHIVE="${WORK_DIR}/raspbian.zip"
+IMAGE="${WORK_DIR}/raspbian.img"
+HASH="cb799af077930ff7cbcfaa251b4c6e25b11483de"
+TARGET="disk2"
 
-ZIP="${WORK_DIR}/raspbian.zip"
-if [ ! -f "${ZIP}" ]; then
-    wget http://downloads.raspberrypi.org/raspbian_latest -O ${ZIP}
+if [ ! -f "${ARCHIVE}" ]; then
+    wget http://downloads.raspberrypi.org/raspbian_latest -O ${ARCHIVE}
 fi
 
-IMG="${WORK_DIR}/raspbian.img"
-if [ ! -f "${IMG}" ]; then
-    unzip ${ZIP} -d ${IMG}
+if [ ! -f "${IMAGE}" ]; then
+    unzip -p "${ARCHIVE}" "*.img" | cat > "${IMAGE}"
 fi
 
-diskutil unmountDisk /dev/disk2
-sudo diskutil partitionDisk /dev/disk2 1 MBR "Free Space" "%noformat%" 100%
-sudo dd bs=1m if=${IMG} of=/dev/rdisk2
-sudo diskutil eject /dev/rdisk2
+CHECKSUM=$(sha1sum ${IMAGE} | awk '{ print $1 }' )
+
+if [ "${CHECKSUM}" = "${HASH}" ]; then
+    echo "Checksum failed."
+
+    exit 1
+fi
+
+diskutil list
+echo "TARGET: ${TARGET}"
+echo "Continue? [y/N]"
+read CONTINUE
+
+if [ ! "${CONTINUE}" = "y" ]; then
+    echo "User abort."
+
+    exit 1
+fi
+
+diskutil unmountDisk "/dev/${TARGET}"
+sudo diskutil partitionDisk "/dev/${TARGET}" 1 MBR "Free Space" %noformat% 100%
+sudo dd bs=1m "if=${IMAGE}" "of=/dev/r${TARGET}"
+sudo diskutil eject "/dev/r${TARGET}"
